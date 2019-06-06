1912f36cedff4c6e544ba5d9bf467862e52c63e3
Nastya <33703310+nanastv@users.noreply.github.com>
2019-06-06 15:32:43 +0900

    Add files via upload

---
+++ EnemyMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EnemyMove : MonoBehaviour
+{
+    NavMeshAgent enemy;
+    public Transform Raspologenie;
+    public Text ttxx;
+    static int score = 0;
+
+    void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+        ttxx.text = "Score:" + score;
+    }
+
+    void Update()
+    {
+        enemy.SetDestination(Raspologenie.position);
+    }
+
+    void OnCollisionEnter(Collision net)
+    {	if(net.gameObject.tag == "Bullet"){
+    		Destroy(gameObject);
+            score = score + 1;
+            ttxx.text = "Score:" + score;
+            if(score == 4) {
+            SceneManager.LoadScene(0);
+            }
+    	}
+
+    }
+}
---
+++ MovePlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MovePlayer : MonoBehaviour
+{
+    public GameObject player;
+    Rigidbody rb;
+    public GameObject bullet;
+    GameObject clone;
+    Rigidbody rbClone;
+    int hp = 10;
+    public Text txt;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        txt.text = "HP:" + hp;
+    }
+
+    void Update()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        rb.AddForce(transform.forward * moveVertical * 50f);
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(0f, moveHorizontal* 5f,0f);
+
+        if(Input.GetKeyDown("space")){
+        	clone = Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        	rbClone = clone.GetComponent<Rigidbody>();
+        	rbClone.AddForce(transform.forward * 300f);
+        }
+    }
+
+    void OnCollisionEnter(Collision da)
+    {	if(da.gameObject.tag == "Enemy"){
+    		hp = hp - 1;
+            txt.text = "HP:" + hp;
+            if (hp == 0){
+            SceneManager.LoadScene(1);
+            }
+  		}
+
+    }
+}
